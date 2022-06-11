7e8856c59329ee78b386f50a1675af99a671df76
klaercio <klaerciosilva@gmail.com>
2022-06-11 13:05:18 -0300

    📓Listagem dos restaurantes na página de administração concluída

--- src/componentes/ListaRestaurantes/Restaurante/index.tsx
+++ src/componentes/ListaRestaurantes/Restaurante/index.tsx
@@ -1,6 +1,9 @@
+import { useEffect, useState } from 'react';
 import IRestaurante from '../../../interfaces/IRestaurante';
 import Prato from '../Prato';
 import estilos from './Restaurante.module.scss';
+import Iprato from '../../../interfaces/IPrato';
+import axios from 'axios';
 
 interface RestauranteProps {
   restaurante: IRestaurante
@@ -8,12 +11,22 @@ interface RestauranteProps {
 
 const Restaurante = ({ restaurante }: RestauranteProps) => {
 
+  const [pratos, setPratos] = useState<Iprato[]>([]);
+
+  useEffect(()=> {
+    axios.get<Iprato[]>(`http://localhost:8000/api/v1/restaurantes/${restaurante.id}/pratos/`)
+    .then(response => {
+      setPratos(response.data);
+      console.log(response.data);
+    })
+  },[restaurante.id])
+
   return (<section className={estilos.Restaurante}>
     <div className={estilos.Titulo}>
       <h2>{restaurante.nome}</h2>
     </div>
     <div>
-      {restaurante.pratos?.map(item => <Prato prato={item} key={item.id} />)}
+      {pratos?.map(item => <Prato prato={item} key={item.id} />)}
     </div>
   </section>)
 }
