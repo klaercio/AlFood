017d28685d6fc0368d21e80b4c8d43749c3bcde9
Klaercio <klaerciosilva@gmail.com>
2022-06-18 13:06:27 -0300

    🚀Função editar restauranete finalizada

--- src/paginas/Administracao/NovoRestaurante/index.tsx
+++ src/paginas/Administracao/NovoRestaurante/index.tsx
@@ -1,13 +1,32 @@
 import { Button, TextField } from "@mui/material";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import IRestaurante from "../../../interfaces/IRestaurante";
 
 export default function NovoRestaurante() {
+
+
+    const parametro = useParams();
+
+    useEffect(()=> {
+        if (parametro.id) {
+            axios.get<IRestaurante>(`http://127.0.0.1:8000/api/v2/restaurantes/${parametro.id}/`)
+            .then(response => setRestaurante(response.data.nome));
+        }
+    },[parametro]);
+
 
     const [restaurante, setRestaurante] = useState('');
 
     function aoSubmit(evento : React.FormEvent<HTMLFormElement>) {
         evento.preventDefault();
+
+        if(parametro.id) {
+            axios.put(`http://127.0.0.1:8000/api/v2/restaurantes/${parametro.id}/`, {nome : restaurante})
+            .then(()=> alert('usuario editado com sucesso'));
+            return;
+        }
 
         axios.post('http://localhost:8000/api/v2/restaurantes/', {nome : restaurante})
         .then(()=> (
