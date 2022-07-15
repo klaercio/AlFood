62d1c1e640de00b81628991e0602827be48c1672
Klaercio <klaerciosilva@gmail.com>
2022-07-14 21:47:21 -0300

    🚀Header das páginas de administração criadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import NovoRestaurante from './paginas/Administracao/NovoRestaurante';
 import AdministracaoRestaurantes from './paginas/Administracao/Restaurantes';
+import PaginaBaseAdmin from './paginas/Administracao/Restaurantes/PaginaBaseAdmin';
 import Home from './paginas/Home';
 import VitrineRestaurantes from './paginas/VitrineRestaurantes';
 
@@ -10,9 +11,13 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
-      <Route path="/admin/restaurantes" element={<AdministracaoRestaurantes />} />
-      <Route path="/admin/restaurantes/novo" element={<NovoRestaurante />} />
-      <Route path="/admin/restaurantes/:id" element={<NovoRestaurante />} />
+
+
+      <Route path="/admin" element={<PaginaBaseAdmin/>}>
+        <Route path="restaurantes" element={<AdministracaoRestaurantes />} />
+        <Route path="restaurantes/novo" element={<NovoRestaurante />} />
+        <Route path="restaurantes/:id" element={<NovoRestaurante />} />
+      </Route>
     </Routes>
   );
 }
--- src/paginas/Administracao/NovoRestaurante/index.tsx
+++ src/paginas/Administracao/NovoRestaurante/index.tsx
@@ -1,48 +1,59 @@
-import { Button, TextField, Typography } from "@mui/material";
+import { AppBar, Button, Container, Link, Paper, TextField, Toolbar, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import http from "../../../http";
 import IRestaurante from "../../../interfaces/IRestaurante";
+import {Link as RouterLink} from 'react-router-dom';
 
 export default function NovoRestaurante() {
 
 
     const parametro = useParams();
 
-    useEffect(()=> {
+    useEffect(() => {
         if (parametro.id) {
             axios.get<IRestaurante>(`http://127.0.0.1:8000/api/v2/restaurantes/${parametro.id}/`)
-            .then(response => setRestaurante(response.data.nome));
+                .then(response => setRestaurante(response.data.nome));
         }
-    },[parametro]);
+    }, [parametro]);
 
 
     const [restaurante, setRestaurante] = useState('');
 
-    function aoSubmit(evento : React.FormEvent<HTMLFormElement>) {
+    function aoSubmit(evento: React.FormEvent<HTMLFormElement>) {
         evento.preventDefault();
 
-        if(parametro.id) {
-            http.put(`restaurantes/${parametro.id}/`, {nome : restaurante})
-            .then(()=> alert('restaurante editado com sucesso!!'));
+        if (parametro.id) {
+            http.put(`restaurantes/${parametro.id}/`, { nome: restaurante })
+                .then(() => alert('restaurante editado com sucesso!!'));
             return;
         }
 
-        http.post('restaurantes/', {nome : restaurante})
-        .then(()=> (
-            alert('restaurante cadastrado com sucesso!!')
-        ))
+        http.post('restaurantes/', { nome: restaurante })
+            .then(() => (
+                alert('restaurante cadastrado com sucesso!!')
+            ))
     }
 
     return (
-        <Box sx={{display: 'flex', flexDirection: 'column', alignItems:"center"}}>
-            <Typography component="h1" variant="h6">Formulário de restaurantes</Typography>
-            <Box component="form" onSubmit={aoSubmit}>
-                <TextField id="standard-basic" value={restaurante} onChange={evento => setRestaurante(evento.target.value)} label="Nome do Restaurante" fullWidth required/>
-                <Button sx={{marginTop: 1}}type="submit" variant="outlined" fullWidth>Salvar</Button>
+        <>
+            <Box>
+                <Container maxWidth="lg" sx={{ mt: 1 }}>
+                    <Paper sx={{ p: 2 }}>
+                        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: "center", flexGrow: 1}}>
+                            <Typography component="h1" variant="h6">Formulário de restaurantes</Typography>
+                            <Box component="form" onSubmit={aoSubmit} sx={{width: '100%'}}>
+                                <TextField id="standard-basic" value={restaurante} onChange={evento => setRestaurante(evento.target.value)} label="Nome do Restaurante" fullWidth required />
+                                <Button sx={{ marginTop: 1 }} type="submit" variant="outlined" fullWidth>Salvar</Button>
+                            </Box>
+                        </Box>
+                    </Paper>
+                </Container>
             </Box>
-        </Box>
+
+
+        </>
     );
 }
