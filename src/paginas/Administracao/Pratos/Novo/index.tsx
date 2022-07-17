35e529ba8cb40e89e0ac2d3b67dc18466565e31d
Klaercio <klaerciosilva@gmail.com>
2022-07-16 22:20:36 -0300

    🚀Adicionado novos campos no cadastro de prato

--- src/paginas/Administracao/Pratos/Novo/index.tsx
+++ src/paginas/Administracao/Pratos/Novo/index.tsx
@@ -1,12 +1,11 @@
-import { Button, Container, Paper, TextField, Typography } from "@mui/material";
+import { Button, Container, FormControl, InputLabel, MenuItem, Paper, Select, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Prato from "../../../../componentes/ListaRestaurantes/Prato";
 import http from "../../../../http";
 import IPrato from "../../../../interfaces/IPrato";
-import IRestaurante from "../../../../interfaces/IRestaurante";
+import ITag from "../../../../interfaces/ITag";
 
 export default function NovoRestaurante() {
 
@@ -19,20 +18,29 @@ export default function NovoRestaurante() {
                 .then(response => {
                     setPrato(response.data.nome);
                     setDescricao(response.data.descricao);
+                    setRestaurante(response.data.restaurante);
                     });
         }
     }, [parametro]);
 
-
+    
+    
     const [prato, setPrato] = useState('');
     const [descricao, setDescricao] = useState('');
     const [restaurante, setRestaurante] = useState<Number>();
+    const [tags, setTags] = useState<ITag[]>([]);
+    const [tag, setTag] = useState('');
+    
+    useEffect(()=> {
+        http.get<{ tags: ITag[]}>('tags/')
+        .then(response => setTags(response.data.tags));
+    },[]);
 
     function aoSubmit(evento: React.FormEvent<HTMLFormElement>) {
         evento.preventDefault();
 
         if (parametro.id) {
-            axios.put(`http://127.0.0.1:8000/api/v2/pratos/${parametro.id}/`, { nome: prato, descricao: descricao, restaurante: 2 })
+            axios.put(`http://127.0.0.1:8000/api/v2/pratos/${parametro.id}/`, { nome: prato, descricao: descricao, restaurante: restaurante })
                 .then(() => alert('Prato editado com sucesso!!'));
             return;
         }
@@ -51,8 +59,18 @@ export default function NovoRestaurante() {
                         <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: "center", flexGrow: 1}}>
                             <Typography component="h1" variant="h6">Formulário de prato</Typography>
                             <Box component="form" onSubmit={aoSubmit} sx={{width: '100%'}}>
-                                <TextField sx={{ marginTop: 1 }}id="standard-basic" value={prato} onChange={evento => setPrato(evento.target.value)} label="Nome do prato" fullWidth required />
+                                <TextField sx={{ marginTop: 1 }} id="standard-basic" value={prato} onChange={evento => setPrato(evento.target.value)} label="Nome do prato" fullWidth required />
                                 <TextField sx={{ marginTop: 1 }} id="standard-basic" value={descricao} onChange={evento => setDescricao(evento.target.value)} label="Descrição do prato" fullWidth required />
+                                <FormControl margin="dense" fullWidth>
+                                    <InputLabel id="selectTag">Tag</InputLabel>
+                                    <Select labelId="selectTag" value={tag} onChange={event => setTag(event.target.value)}>
+                                        {tags.map(tag => {
+                                            <MenuItem value={tag.id} key={tag.id}>
+                                                {tag.value}
+                                            </MenuItem>
+                                        })}
+                                    </Select>
+                                </FormControl>
                                 <Button sx={{ marginTop: 1 }} type="submit" variant="outlined" fullWidth>Salvar</Button>
                             </Box> 
                         </Box>
